fe1700bfed7dde12ecd2513d5c9aa81a958cbcde
SovietFalcon <ivancp3000@hotmail.es>
2020-09-29 18:38:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/contadordeclics/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.contadordeclics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
+
+}
